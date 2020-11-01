77f9b4965e06ab0acc917cb774ade3661767a132
Dominic Grishajev <dgrishajev@gmail.com>
2020-11-01 12:29:47 +0200

    add submission for exercises 3.7-3.8 of part3

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('body', req => JSON.stringify(req.body))
+
 app.use(express.json())
+app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+  skip: ({ method }) => method !== 'POST'
+}))
 
 let persons = [{
   id: 1,
